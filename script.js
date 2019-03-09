82bb2aefc50bd151fe8226edd8fd8383cbc628f7
Mariusz Domanski <mariusz@domanski.pl>
2019-03-09 18:25:54 +0100

    Create script

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-var app = React.createElement(App);
-ReactDOM.render(app, document.getElementById('app'));
+var app = React.createElement(timer);
+ReactDOM.render(timer, document.getElementById('app'));
